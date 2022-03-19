9ed9b8d8a0c343d1400c69fbe9e6dee49ee0129a
TajalTechnology <mrpresident1189@gmail.com>
2022-03-20 01:18:45 +0600

    update with class correction added test

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,5 +1,4 @@
 import { Strikeing } from '../index';
 test('My Greeter', () => {
   const strikeing = new Strikeing();
-  expect(strikeing.play()).toBe('Welcome to the class');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
+const fs = require('fs')
+const S3 = require('aws-sdk/clients/s3')
+const path = require("path");
+
 interface IPlay {
-  play(): void;
 }
 
 export class Strikeing implements IPlay {
-  play() {
-    return 'Welcome to the class';
+  play(creadentials: any, file: any) {
+
+    const accessKeyId = creadentials.accessKeyId;
+    const secretAccessKey = creadentials.secretAccessKey;
+    const region = creadentials.region;
+    const bucketName = creadentials.bucketName;
+
+    const fileStream = fs.createReadStream(file.path);
+
+    const uploadParams = {
+      Bucket: bucketName,
+      Body: fileStream,
+      Key: file.filename
+    };
+    const s3 = new S3({
+      region,
+      accessKeyId,
+      secretAccessKey
+    });
+
+    return s3.upload(uploadParams).promise()
   }
 }
