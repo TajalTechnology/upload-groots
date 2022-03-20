15da905305ba9970c11441e4752b3d8fd208fbbc
TajalTechnology <mrpresident1189@gmail.com>
2022-03-20 14:34:54 +0600

    update version

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,22 @@
 import fs = require('fs');
 import S3 = require('aws-sdk/clients/s3');
 import path = require('path');
+import { IFileUpload } from './interface';
 
-interface IPlay {
-  pulay(): void;
-}
-
-export class Strikeing implements IPlay {
-  pulay(): void {
-    throw new Error('Method not implemented.');
-  }
-  play(creadentials: any, file: any) {
+export class AWS implements IFileUpload {
+  uploadFile(creadentials: any, file: any) {
     const accessKeyId = creadentials.accessKeyId;
     const secretAccessKey = creadentials.secretAccessKey;
     const region = creadentials.region;
     const bucketName = creadentials.bucketName;
-
     const fileStream = fs.createReadStream(file.path);
 
     const uploadParams = {
       Bucket: bucketName,
       Body: fileStream,
       Key: file.filename,
-    };
+    }; 
+
     const s3 = new S3({
       region,
       accessKeyId,
---
+++ src/interface.ts
@@ -0,0 +1,3 @@
+export interface IFileUpload {
+     uploadFile(creadentials: any, file: any): any;
+}
